7318f8eb230e697039321e24cb296e9eb2f38936
Santiago Smith <sssilvar@unal.edu.co>
2019-06-16 18:48:34 -0500

    Some changes

--- preprocessing/recon_all.py
+++ preprocessing/recon_all.py
@@ -19,12 +19,16 @@ if __name__ == "__main__":
 
     # Output Folder
     out_folder = '/home/jullygh/Downloads/MS/processed_fs/'
-    # if not isdir(out_folder):
-    #     os.mkdir(out_folder)
 
     # Find files in folder
     files = glob.glob(pattern, recursive=True)
-    for f in files:
-        recon_all(f)
-
     print(f'Total files found: {len(files)}')
+    confirm = input('Start [y/n]:')
+    
+    if confirm == 'y':
+        # Process subjects in parallel
+        pool = Pool(20)
+        pool.map(recon_all, files)
+    else:
+        print('No process started')
+    print('Done')
