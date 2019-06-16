87a7ebe8e6f58a172082665af6a52a06c1faf22c
Santiago Smith <sssilvar@unal.edu.co>
2019-06-16 18:31:12 -0500

    Some changes

--- preprocessing/recon_all.py
+++ preprocessing/recon_all.py
@@ -5,9 +5,11 @@ from os.path import join
 if __name__ == "__main__":
     # Set dataset folder
     dataset_folder = '/home/jullygh/Downloads/MS/extracted'
+    pattern = join(dataset_folder, '*.nii.gz')
+    print(f'Finging pattern: {pattern}')
 
     # Find files in folder
-    files = glob.glob(join(dataset_folder, '*.nii.gz'))
+    files = glob.glob(pattern)
     for f in files:
         print(f)
 
