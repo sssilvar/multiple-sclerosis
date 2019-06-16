7fc6d02dbaef1967ed6be69223ee9fc090135921
Santiago Smith <sssilvar@unal.edu.co>
2019-06-16 18:48:44 -0500

    Some changes

--- preprocessing/recon_all.py
+++ preprocessing/recon_all.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
         # Process subjects in parallel
         pool = Pool(20)
         pool.map(recon_all, files)
+        pool.close()
     else:
         print('No process started')
     print('Done')
