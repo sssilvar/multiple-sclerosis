6f7b7f7049d51bc7172749d5c9f13f4e1367a77f
Santiago Smith <sssilvar@unal.edu.co>
2019-06-16 18:35:57 -0500

    Some changes

--- preprocessing/recon_all.py
+++ preprocessing/recon_all.py
@@ -5,7 +5,7 @@ from os.path import join
 if __name__ == "__main__":
     # Set dataset folder
     dataset_folder = '/home/jullygh/Downloads/MS/extracted/*'
-    pattern = join(dataset_folder, 'patient*_study1_T1W.nii.gz')
+    pattern = join(dataset_folder, 'patient*_study1_T1Wreg.nii.gz')
     print(f'Finging pattern: {pattern}')
 
     # Find files in folder
